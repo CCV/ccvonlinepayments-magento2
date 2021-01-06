5a211e326f76a04ef6f5ebf73a903874e5256f3b
PeterCMDC <peter@cmdc.eu>
2021-01-06 13:59:11 +0100

    Rename package / Update versions / Update url

--- copyImages.sh
+++ copyImages.sh
@@ -3,5 +3,5 @@ rm -Rf $(pwd)/view/base/web/images
 mkdir $(pwd)/view/base/web/images
 mkdir $(pwd)/view/base/web/images/methods
 
-cp -Rf $(pwd)/vendor/ccvonlinepayments/images/*.png $(pwd)/view/base/web/images
-cp -Rf $(pwd)/vendor/ccvonlinepayments/images/methods/*.png $(pwd)/view/base/web/images/methods
+cp -Rf $(pwd)/vendor/ccv/images/*.png $(pwd)/view/base/web/images
+cp -Rf $(pwd)/vendor/ccv/images/methods/*.png $(pwd)/view/base/web/images/methods
